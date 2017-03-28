a68c8ecd9b6eb0068f7fc42222770cd0d80e839f
Denton Liu <liu.denton@gmail.com>
2017-03-28 00:26:33 -0700

    Reformat boot.s

--- boot.s
+++ boot.s
@@ -1,31 +1,31 @@
-		.set ALIGN,		1 << 0
-		.set MEMINFO,		1 << 1
-		.set FLAGS,		ALIGN | MEMINFO
-		.set MAGIC,		0x1BADB002
-		.set CHECKSUM,		-(MAGIC + FLAGS)
+		.set	ALIGN, 1 << 0
+		.set	MEMINFO, 1 << 1
+		.set	FLAGS, ALIGN | MEMINFO
+		.set	MAGIC, 0x1BADB002
+		.set	CHECKSUM, -(MAGIC + FLAGS)
 
-		.section .header
-		.balign 4
-		.long MAGIC
-		.long FLAGS
-		.long CHECKSUM
+		.section	.header
+		.balign	4
+		.long	MAGIC
+		.long	FLAGS
+		.long	CHECKSUM
 
-		.section .bss
-		.balign 16
+		.section	.bss
+		.balign	16
 s_bottom:
-		.skip 16384					# Reserve 16 KiB of memory for the stack
+		.skip	16384				# Reserve 16 KiB of memory for the stack
 s_top:
 
-		.section .text
-		.global _start
-		.type _start, @function
+		.section	.text
+		.global	_start
+		.type	_start, @function
 _start:
-		mov $s_top, %esp
+		mov	$s_top, %esp
 		# TODO: initialize processor state here
-		call main
+		call	main
 
 		cli
 hltloop:	hlt
-		jmp hltloop
+		jmp	hltloop
 
-		.size _start, . - _start
+		.size	_start, . - _start
--- musikernel.c
+++ musikernel.c
@@ -1,6 +1,18 @@
 #include "terminal.h"
+#include "sys.h"
 
 void main() {
+	int i;
+	char c = 'a';
+	int count = 500;
 	terminal_initialize();
-	terminal_writestring("Hello world!\n");
+	while (count--) {
+		if (c == 'z' + 1) {
+			c = 'a';
+		}
+		terminal_putchar(c++);
+		for (i = 0; i < 100000; i++)
+			;
+	}
+	halt();
 }
