e79b9815ae0bdcb1417e45ced661a6ced3f98a03
Denton Liu <liu.denton@gmail.com>
2017-03-27 21:46:03 -0700

    Change constants to define statements

--- terminal.c
+++ terminal.c
@@ -3,8 +3,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
 
 static size_t terminal_column;
 static size_t terminal_row;
