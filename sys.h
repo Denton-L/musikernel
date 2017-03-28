54a09ae51e2b90cfab2c265e11c58891f05f2eeb
Denton Liu <liu.denton@gmail.com>
2017-03-28 00:31:20 -0700

    Define in* and out*

--- sys.h
+++ sys.h
@@ -1,6 +1,14 @@
 #ifndef SYS_H
 #define SYS_H
 
+#include <stdint.h>
+
 void halt();
+uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
+uint32_t inl(uint16_t port);
+void outb(uint16_t port, uint8_t value);
+void outw(uint16_t port, uint16_t value);
+void outl(uint16_t port, uint32_t value);
 
 #endif
