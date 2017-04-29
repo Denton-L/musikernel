598cc7c3d541028fcddca9eb44727f32f23f0200
Denton Liu <liu.denton@gmail.com>
2017-04-28 23:13:15 -0400

    Define memset

--- string.c
+++ string.c
@@ -8,3 +8,13 @@ size_t strlen(const char *str) {
 		++len;
 	return len;
 }
+
+void *memset(void *ptr, int value, size_t num) {
+	unsigned char real_value = (unsigned char) value;
+	char *real_ptr = (char *) ptr;
+
+	while (num > 0)
+		real_ptr[--num] = real_value;
+
+	return ptr;
+}
--- string.h
+++ string.h
@@ -1,7 +1,9 @@
 #ifndef STRING_H
 #define STRING_H
+
 #include <stddef.h>
 
 size_t strlen(const char *str);
+void *memset(void *ptr, int value, size_t num);
 
 #endif
